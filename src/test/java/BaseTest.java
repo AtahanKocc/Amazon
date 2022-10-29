05f8b6073a8027af1ad64e84f45115d44499b4b7
Atahan Koc <koccata@gmail.com>
2022-10-29 13:52:19 +0300

    Initial commit

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,28 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import testlogger.TestResultLogger;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ExtendWith(TestResultLogger.class)
+public class BaseTest {
+
+    WebDriver driver ;
+
+    @BeforeAll
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.amazon.com.tr/");
+        driver.manage().window().maximize();
+    }
+
+    @AfterAll
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
